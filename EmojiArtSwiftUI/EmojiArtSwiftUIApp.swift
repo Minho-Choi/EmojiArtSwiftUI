aacc2fdf2a7356072b82b046178b9608d83d240f
Minho-Choi <2153373@naver.com>
2021-01-29 14:48:45 +0900

    lecture13 implementation completed

--- EmojiArtSwiftUI/EmojiArtSwiftUIApp.swift
+++ EmojiArtSwiftUI/EmojiArtSwiftUIApp.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 @main
 struct EmojiArtSwiftUIApp: App {
-    let store = EmojiArtDocumentStore(named: "Emoji Art")
+//    let store = EmojiArtDocumentStore(named: "Emoji Art")
+    let store = EmojiArtDocumentStore(directory: FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!)
     var body: some Scene {
         WindowGroup {
             EmojiArtDocumentChooser().environmentObject(store)
